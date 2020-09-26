85a8400ca0339decce57355ca5c7c65a22ee017c
repl.it user <replituser@example.com>
2020-09-26 23:32:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -48,7 +48,7 @@ void insertMap(HashMap * map, char * key, void * value) {
     int pos = hash(key, map->capacity);
     int aux = pos;
     struct Pair *n = (struct Pair*) malloc(sizeof(struct Pair*));
-    while ( (map->buckets[pos]->key != NULL) || (strcmp(map->buckets[pos]->key, key) != 0) ) {
+    while ( (map->buckets[pos]->key != NULL) && (strcmp(map->buckets[pos]->key, key) != 0) ) {
         pos++;
         if (pos > map->capacity) {
             pos = 0;

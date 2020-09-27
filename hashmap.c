de331368a22a6f54634ce80bb4054afe894faec4
repl.it user <replituser@example.com>
2020-09-27 04:19:59 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -85,6 +85,7 @@ void eraseMap(HashMap * map,  char * key) {
     while (map->buckets[pos] != NULL) {
         if (is_equal(map->buckets[pos]->key, key)) {
             map->buckets[pos]->key = NULL;
+            map->size--;
             break;
         }
         pos++;
